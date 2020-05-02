e39b4c3448979e9e1175dc0bccadcd9d8fb8a544
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-05-02 13:59:08 +0300

    Core build

--- build/core.js
+++ build/core.js
@@ -1,1 +1,12 @@
-console.log('core done');
+const copyFonts = require('./shared/copy-fonts');
+const copySCSS = require('./shared/copy-scss');
+const scss = require('./shared/scss');
+
+function build() {
+  // Copy fonts
+  copyFonts('core');
+  copySCSS('core');
+  scss('core');
+}
+
+build();
