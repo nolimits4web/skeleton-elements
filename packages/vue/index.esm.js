b6c25bc87829d4cde26928adf2c552814a2d97d0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-05-04 17:34:24 +0300

    Remove build artifacts

--- packages/vue/index.esm.js
+++
@@ -1,5 +0,0 @@
-import SkeletonBlock from './esm/SkeletonBlock';
-import SkeletonText from './esm/SkeletonText';
-import SkeletonImage from './esm/SkeletonImage';
-import SkeletonAvatar from './esm/SkeletonAvatar';
-export { SkeletonBlock, SkeletonText, SkeletonImage, SkeletonAvatar };
