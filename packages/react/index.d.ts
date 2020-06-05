d0ee11163a0dc3dfbfdc05e9a52d77ee4074e7b1
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-06-05 20:17:02 +0300

    Untrack build files

--- packages/react/index.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import SkeletonAvatar from './types/SkeletonAvatar';
-import SkeletonBlock from './types/SkeletonBlock';
-import SkeletonImage from './types/SkeletonImage';
-import SkeletonText from './types/SkeletonText';
-
-export { SkeletonAvatar, SkeletonBlock, SkeletonImage, SkeletonText };
