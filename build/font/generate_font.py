3f85047e65ae629a7e9e29e53abdc83410fc2f9b
Vlad <vl.tansky@gmail.com>
2021-06-21 09:23:50 +0300

    feat: russian, arabic, hebrew, persian support

--- build/font/generate_font.py
+++ build/font/generate_font.py
@@ -11,6 +11,17 @@ SQUARE_PATH = os.path.join(SCRIPT_PATH, 'square.svg')
 OUTPUT_FONT_DIR = os.path.join(SCRIPT_PATH, '..', '..', 'src/fonts')
 AUTO_WIDTH = False
 KERNING = 0
+def char_range(arr):
+    result = [];
+    for m in arr:
+        txt = m.split("-")
+        for c in range(ord(txt[0]), ord(txt[1])+1):
+            result.append(chr(c))
+    return result
+
+
+def split(word):
+    return [char for char in word]
 
 m = md5.new()
 
@@ -22,7 +33,11 @@ f.ascent = 448
 f.descent = 64
 
 # Import base characters
-for char in "0123456789abcdefghijklmnopqrstuvwzxyzABCDEFGHIJKLMNOPQRSTUVWZXYZ_- .,:;/\!/*&'\"|(){}[]":
+hebrew = '\u0590-\u05fe'
+arabic = '\u0621-\u064A'
+persian = '\u0600-\u06FF'
+special = split("_- .,:;/\!/*&'\"|(){}[]")
+for char in char_range(['a-z', 'A-Z', '1-9', 'а-я', 'А-Я', arabic, hebrew, persian]) + special:
   glyph = f.createChar(ord(char))
   glyph.importOutlines(SQUARE_PATH)
   glyph.width = 256
